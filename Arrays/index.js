e3dc2184d0e22c562a3e935da195d2fbfac9d1e8
Rakib1415 <rakibiuaece@gmail.com>
2023-01-14 00:34:16 +0600

    add all about array data structure

--- Arrays/index.js
+++ Arrays/index.js
@@ -46,38 +46,38 @@ strings.splice(2, 0, 'alien');
 // console.log(wizard1);
 
 
-class MyArray {
-    constructor(){
-        this.length = 0;
-        this.data = {};
-    }
-    push(item){ // TC : O(1)
-        this.data[this.length] = item;
-        this.length++;
-        return this.length
-    }
-    pop(){ // TC : O(1)
-        const lastItem = this.data[this.length - 1];
-        delete this.data[this.length - 1];
-        this.length--;
-        return lastItem;
-    }
-    get(index){ // TC : O(1)
-        return this.data[index];
-    }
-    delete(index){ // TC : O(n)
-        const item = this.data[index];
-        this.shiftItems(index);
-        return item;
-    }
-    shiftItems(index){ // TC : O(n)
-        for(let i = index; i < this.length - 1; i++){
-            this.data[i] = this.data[i+1];
-        }
-        delete this.data[this.length - 1];
-        this.length--;
-    }
-};
+// class MyArray {
+//     constructor(){
+//         this.length = 0;
+//         this.data = {};
+//     }
+//     push(item){ // TC : O(1)
+//         this.data[this.length] = item;
+//         this.length++;
+//         return this.length
+//     }
+//     pop(){ // TC : O(1)
+//         const lastItem = this.data[this.length - 1];
+//         delete this.data[this.length - 1];
+//         this.length--;
+//         return lastItem;
+//     }
+//     get(index){ // TC : O(1)
+//         return this.data[index];
+//     }
+//     delete(index){ // TC : O(n)
+//         const item = this.data[index];
+//         this.shiftItems(index);
+//         return item;
+//     }
+//     shiftItems(index){ // TC : O(n)
+//         for(let i = index; i < this.length - 1; i++){
+//             this.data[i] = this.data[i+1];
+//         }
+//         delete this.data[this.length - 1];
+//         this.length--;
+//     }
+// };
 
 // const newArray = new MyArray();
 // newArray.push('hi');
@@ -90,9 +90,86 @@ class MyArray {
 // newArray.delete(1);
 // console.log(newArray);
 
-const newArray = new MyArray();
-newArray.push(1);
-newArray.push(2);
-newArray.push(3);
-newArray.push(4);
-console.log(newArray);
+// const newArray = new MyArray();
+// newArray.push(1);
+// newArray.push(2);
+// newArray.push(3);
+// newArray.push(4);
+// console.log(newArray);
+
+
+// function reverse(str){
+//     const stringArr = str.split(' ');
+//     let reverseString = '';
+//     for(let i = stringArr.length - 1; i >= 0; i--){
+//         let word = stringArr[i];
+//         for(let j = word.length - 1; j >= 0; j--){
+//            reverseString += word.at(j) 
+//         }
+//         reverseString += ' ';
+//     }
+//     return reverseString;
+// }
+
+// function reverse(str){
+//     let reverseString = '';
+//     for(let i = str.length - 1; i >= 0; i--){
+//         reverseString += str.at(i);
+//     }
+//     return reverseString;
+// }
+
+// function reverse(str){
+//     const backwords = [];
+//     for(let i = str.length - 1; i >= 0; i--){
+//         backwords.push(str[i]);
+//     }
+//     return backwords.join('');
+// }
+
+// const reverse = str => [...str].reverse().join('');
+
+// console.log(reverse('i am a Rakib'));
+
+
+function mergeSortedArrays(firstArr, secondArr){ // SC : O (n + m)   TC : O(n + m)
+    const mergedArr = [];
+
+    let i = 0, j = 0;
+    while(i < firstArr.length && j < secondArr.length){
+        if(firstArr[i] <= secondArr[j]){
+            mergedArr.push(firstArr[i]);
+            i++;
+        }else{
+            mergedArr.push(secondArr[j]);
+            j++;
+        }
+    }
+    if(i < firstArr.length){
+        while(i < firstArr.length){
+            mergedArr.push(firstArr[i]);
+            i++;
+        }
+    }
+
+      if(j < secondArr.length){
+        while(j < secondArr.length){
+            mergedArr.push(secondArr[j]);
+            j++;
+        }
+    }
+
+    return mergedArr;
+}
+
+console.log(mergeSortedArrays([0,3,4,31], [4,6,30]))
+
+// Array pros and cons
+
+// fast lookup
+// fast push or pop
+// ordered
+
+// slow inserts
+// slow deletes
+// fixed size (if use static array)
